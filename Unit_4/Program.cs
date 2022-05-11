8f79c4fc114a66a2f09cb829cf18c1c08ae193a1
Name <erofeev001@gmail.com>
2022-05-11 11:17:31 +0300

    4.4.5 with normal namecount

--- Unit_4/Program.cs
+++ Unit_4/Program.cs
@@ -1,13 +1,14 @@
 ﻿//4.4.5
 (string Name, string Type, double Age, int NameCount) Pet;
-Pet.NameCount = 0;
+
 Console.WriteLine("Enter Name");
 Pet.Name = Console.ReadLine();
 Console.WriteLine("Enter Type");
 Pet.Type = Console.ReadLine();
 Console.WriteLine("Enter Age");
 Pet.Age = Convert.ToDouble(Console.ReadLine());
-Pet.NameCount ++;
+Pet.NameCount = Pet.Name.Length;
+
 Console.WriteLine("NameCount: " + Pet.NameCount);
 Console.WriteLine("Name: " + Pet.Name);
 Console.WriteLine("Type: " + Pet.Type);
