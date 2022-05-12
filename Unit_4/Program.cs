aeab719f85d316da9e8fd70e932cb346c1668076
Name <erofeev001@gmail.com>
2022-05-12 12:44:14 +0300

    4.5

--- Unit_4/Program.cs
+++ Unit_4/Program.cs
@@ -1,16 +1,42 @@
-﻿//4.4.5
-(string Name, string Type, double Age, int NameCount) Pet;
+﻿//4.5
 
-Console.WriteLine("Enter Name");
-Pet.Name = Console.ReadLine();
-Console.WriteLine("Enter Type");
-Pet.Type = Console.ReadLine();
-Console.WriteLine("Enter Age");
-Pet.Age = Convert.ToDouble(Console.ReadLine());
-Pet.NameCount = Pet.Name.Length;
+    (string Name, string Surname, string Login, double LogLength, bool Pet, double Age, string [] FavColors) User;
+for (int k = 0; k < 3; k++)
+{
+    Console.WriteLine("Введите имя");
 
-Console.WriteLine("NameCount: " + Pet.NameCount);
-Console.WriteLine("Name: " + Pet.Name);
-Console.WriteLine("Type: " + Pet.Type);
-Console.WriteLine("Age: " + Pet.Age);
-Console.ReadKey();
+    User.Name = Console.ReadLine();
+
+    Console.WriteLine("Введите фамилию");
+
+    User.Surname = Console.ReadLine();
+
+    Console.WriteLine("Введите логин");
+
+    User.Login = Console.ReadLine();
+
+    User.LogLength = User.Login.Length;
+
+
+    Console.WriteLine("Есть ли у вас животные? Да или Нет");
+
+    string answer = Console.ReadLine();
+
+    if (answer == "Да")
+        User.Pet = true;
+    else
+        User.Pet = false;
+
+    Console.WriteLine("Введите возраст пользователя");
+    User.Age = double.Parse(Console.ReadLine());
+
+    User.FavColors = new string[3];
+
+    Console.WriteLine("Введите три любимых цвета пользователя");
+
+    for (int i = 0; i < 3; i++)
+    {
+        User.FavColors[i] = Console.ReadLine();
+
+    }
+}
